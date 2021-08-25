e44b038a22a53d151135c14b7edee1c8aa2f86b2
Đặng Xuân Danh <57908643+DangXuanDanh@users.noreply.github.com>
2021-08-25 21:29:18 +0700

    update profile

--- src/component/host/profile.js
+++ src/component/host/profile.js
@@ -4,27 +4,71 @@ import { Form, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Login() {
-
+    const saved = localStorage.getItem("user");
+    const initial = JSON.parse(saved);
+    const [profile, setprofile] = useState([]);
+    useEffect(async () => {
+        await fetch("https://nhatrovn.herokuapp.com/api/user/information/" + initial.id, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": 'application/json'
+            },
+        }).then(async function (response) {
+            const result = await response.json()
+            setprofile(result)
+            console.log(result)
+        }).catch((error) => {
+            return error;
+        });
+    }, [])
 
     return (
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-sm-6 col-md-6">
-                    <div class="well well-sm">
-                        <div class="row">
-                            <div >
-                                <h4>
-                                    Bhaumik Patel</h4>
-                                <small><cite title="San Francisco, USA">San Francisco, USA <i class="glyphicon glyphicon-map-marker">
-                                </i></cite></small>
-                                <p>
-                                    <i class="glyphicon glyphicon-envelope"></i>email@example.com
-                                    <br />
-                                    <i class="glyphicon glyphicon-globe"></i><a href="http://www.jquery2dotnet.com">www.jquery2dotnet.com</a>
-                                    <br />
-                                    <i class="glyphicon glyphicon-gift"></i>June 02, 1988</p>
-                            </div>
-                        </div>
+
+        <div class="profile">
+            <div class="d-flex justify-content-between align-items-center mb-3">
+                <h4 class="text-right">Thông Tin Tài Khoản</h4>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <h6 class="mb-0">Họ Tên</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                        {profile.name}
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-3">
+                        <h6 class="mb-0">Email</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                        {profile.email}
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-3">
+                        <h6 class="mb-0">Điện Thoại</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                        {profile.phone}
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-3">
+                        <h6 class="mb-0">CMND</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                        {profile.cardId}
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-12">
+                        <button class="btn btn-info " target="__blank">Edit</button>
                     </div>
                 </div>
             </div>
