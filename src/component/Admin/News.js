6072a27072cc61301e5d7c82df291239c8bd38ff
LOAN Ngo <mrs.loan1999@gmail.com>
2021-08-28 17:29:39 +0700

    Admin - Check common layouts

--- src/component/Admin/News.js
+++ src/component/Admin/News.js
@@ -1,28 +1,42 @@
 import React, {useState, useEffect} from 'react';
-import {FaSearch} from "react-icons/fa";
 import CardItem from "./partials/cardItem";
+import Header from "./partials/header";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
+const news = [
+    {
+        id: 1,
+        title: 'Cho thuê phòng đẹp như khách sạn',
+        image: 'room_1.jpeg',
+        roomID: 1,
+        price: '3.000.000 vnd'
+    },
+    {
+        id: 3,
+        title: 'Cho thuê phòng đẹp như khách sạn',
+        image: 'room_3.jpeg',
+        roomID: 3,
+        price: '2.000.000 vnd'
+    }
+]
 
 function News() {
     return (
         <>
-            <div id="admin-banner">
-                <div className="admin-banner p-2 pl-2 mt-4 mb-2 rounded-3 bold text-white bg-dark text-uppercase">
-                    Quản lý tin
-                </div>
-                <div className="admin-header d-flex justify-content-between rounded bg-light p-2">
-                    <div className="header-links btn-group" role="group">
-                        <button type="button" className="btn btn-secondary" disabled>Danh sách tin</button>
+            <Header title={' Quản lý tin'}/>
+            <Tabs className="admin-tabs mt-2">
+                <TabList>
+                    <Tab>Danh sách tin</Tab>
+                </TabList>
+
+                <TabPanel>
+                    <div className="container py-5">
+                        {news.map((item, i) => (
+                            <CardItem src={item.image} title={item.title} subTitle={item.price} btnText={'Chi tiết'}/>
+                        ))}
                     </div>
-                    <div className="input-group search-field w-25">
-                        <input type="search" className="form-control rounded" placeholder="Search" aria-label="Search"
-                               aria-describedby="search-addon"/>
-                        <span className="input-group-text border-0" id="search-addon"><FaSearch/></span>
-                    </div>
-                </div>
-            </div>
-            <div className="container py-5">
-                <CardItem src={'room_1.jpeg'} title={'Da thanh toan 10tr cho hoa don'} subTitle={'abc'} btnText={'Chi tiết'}/>
-            </div>
+                </TabPanel>
+            </Tabs>
         </>
     )
 };
--- src/component/Admin/partials/cardItem.js
+++ src/component/Admin/partials/cardItem.js
@@ -1,4 +1,4 @@
-function CardItem({src, title, subTitle, btnText, linkBtn = '#'}) {
+function CardItem({src, title, subTitle, btnText, linkBtn = '#', subTitle1}) {
     return (
         <div className="row border border-secondary p-2 rounded-3 shadow-sm mb-4">
             <div className="col-2 p-0">
@@ -7,6 +7,7 @@ function CardItem({src, title, subTitle, btnText, linkBtn = '#'}) {
             <div className="col-7">
                 <h3 className="text-w-dots">{title}</h3>
                 <span className="text-h-dots">{subTitle}</span>
+                <span className="text-h-dots">{subTitle1}</span>
             </div>
             <div className="col-3 p-2 d-flex justify-content-end align-items-end">
                 <a href={linkBtn} className="btn btn-default text-white" variant="primary">
