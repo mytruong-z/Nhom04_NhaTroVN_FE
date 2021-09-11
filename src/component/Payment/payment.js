f09c0096b26b4fb167a1815ebdf38ff4cf44da96
mytruong-z <my.truong@ceresolutions.com>
2021-09-11 16:30:09 +0700

    Clean code

--- src/component/Payment/payment.js
+++ src/component/Payment/payment.js
@@ -1,95 +1,99 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { Component, useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { Button, Modal, Card, Table, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './payment.css';
 
-function Payment() {
+function Payment () {
 
-    const saved = localStorage.getItem("user");
+    const saved = localStorage.getItem('user');
     const initial = JSON.parse(saved);
     const [userID, setId] = useState([initial.id]);
-    const [Notification, setNotification] = useState("");
+    const [Notification, setNotification] = useState('');
     const [showNotification, setShowNotification] = useState(false);
 
-    const history = useHistory()
+    const history = useHistory();
 
     useEffect(() => {
         if (!localStorage.getItem('user')) {
-            history.push("/")
+            history.push('/');
         }
-    }, [])
+    }, []);
 
     const showNotificationModal = () => {
         setShowNotification(true);
-    }
+    };
 
     const hideNotificationModal = () => {
         setShowNotification(false);
-    }
+    };
 
-    async function payment(sub_id) {
-        let item = { userID, sub_id }
-        console.log(JSON.stringify(item))
-        await fetch("https://nhatrovn.herokuapp.com/api/payment/submit", {
+    async function payment (sub_id) {
+        let item = {userID, sub_id};
+        console.log(JSON.stringify(item));
+        await fetch('https://nhatrovn.herokuapp.com/api/payment/submit', {
             method: 'POST',
             headers: {
-                "Content-Type": "application/json",
-                "Accept": 'application/json'
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
             },
             body: JSON.stringify(item)
         }).then(async function (response) {
-            const result = await response.json()
-            setNotification("Hóa đơn "+result.uuid+" đã được ghi nhận chờ xử lý")
-            showNotificationModal()
+            const result = await response.json();
+            setNotification('Hóa đơn ' + result.uuid + ' đã được ghi nhận chờ xử lý');
+            showNotificationModal();
 
         }).catch(function (error) {
-            setNotification("Thanh toán trước đó chưa được xử lý vui lòng chờ")
-            showNotificationModal()
+            setNotification('Thanh toán trước đó chưa được xử lý vui lòng chờ');
+            showNotificationModal();
         });
 
     }
+
     return (
         <div>
-            <div class="lasagna">
-                <div class="upgrade">
-                    <h2>Mua quyền <b>Đăng Nhà trọ</b> </h2>
+            <div className="lasagna">
+                <div className="upgrade">
+                    <h2>Mua quyền <b>Đăng Nhà trọ</b></h2>
                     <p>Để có thể tăng số lượng và thời gian bài đăng</p>
                 </div>
-                <div class="plan">
+                <div className="plan">
                     <h3>Gói Đồng</h3>
                     <p>$199.000 vnd</p>
-                    <Button className="mt-3 mb-3 btn btn-default text-white " onClick={() => { payment(2); }}>Mua</Button>
+                    <Button className="mt-3 mb-3 btn btn-default text-white "
+                            onClick={() => { payment(2); }}>Mua</Button>
                     &nbsp;
-                    <div class="includes">
+                    <div className="includes">
                         Includes
                         <ul>
-                            <li>Có thể đăng và duy trì bài đăng trong 3 tháng </li>
+                            <li>Có thể đăng và duy trì bài đăng trong 3 tháng</li>
                         </ul>
                     </div>
                 </div>
-                <div class="plan">
+                <div className="plan">
                     <h3>Gói Bạc</h3>
                     <p>$399.000 vnd</p>
                     <p></p>
-                    <Button className="mt-3 mb-3 btn btn-default text-white " onClick={() => { payment(3); }}>Mua</Button>
+                    <Button className="mt-3 mb-3 btn btn-default text-white "
+                            onClick={() => { payment(3); }}>Mua</Button>
                     &nbsp;
-                    <div class="includes">
+                    <div className="includes">
                         includes
                         <ul>
-                            <li>Có thể đăng và duy trì bài đăng trong 5 tháng </li>
+                            <li>Có thể đăng và duy trì bài đăng trong 5 tháng</li>
                         </ul>
                     </div>
                 </div>
-                <div class="plan">
+                <div className="plan">
                     <h3>Gói Vàng</h3>
                     <p>$599.000 vnd</p>
-                    <Button className="mt-3 mb-3 btn btn-default text-white " onClick={() => { payment(4); }}>Mua</Button>
+                    <Button className="mt-3 mb-3 btn btn-default text-white "
+                            onClick={() => { payment(4); }}>Mua</Button>
                     &nbsp;
-                    <div class="includes">
+                    <div className="includes">
                         includes
                         <ul>
-                            <li>Có thể đăng và duy trì bài đăng trong 12 tháng </li>
+                            <li>Có thể đăng và duy trì bài đăng trong 12 tháng</li>
                         </ul>
                     </div>
                 </div>
@@ -103,19 +107,17 @@ function Payment() {
                 keyboard={false}
             >
                 <Modal.Header closeButton>
-                    <Modal.Title  className="mt-0 mb-0"><h4 className="float-left">Thông báo</h4></Modal.Title>
+                    <Modal.Title className="mt-0 mb-0"><h4 className="float-left">Thông báo</h4></Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <Form className="mt-3 mb-3">
-                        <Form.Group className="mb-3" controlId="formBasicName" >
+                        <Form.Group className="mb-3" controlId="formBasicName">
                             <Form.Label className="float-left">{Notification}</Form.Label>
                         </Form.Group>
                     </Form>
                 </Modal.Body>
-               
-            </Modal>
 
-            
+            </Modal>
         </div>
     );
 }
