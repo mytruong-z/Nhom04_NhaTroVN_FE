5e0571690141eb7822ccfc6d9959f75a596efd4f
LOAN Ngo <mrs.loan1999@gmail.com>
2021-09-13 01:27:34 +0700

    Admin - News Manage - Fetch data

--- src/component/Admin/News.js
+++ src/component/Admin/News.js
@@ -2,25 +2,51 @@ import React, {useState, useEffect} from 'react';
 import CardItem from "./partials/cardItem";
 import Header from "./partials/header";
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-
-const news = [
-    {
-        id: 1,
-        title: 'Cho thuê phòng đẹp như khách sạn',
-        image: 'room_1.jpeg',
-        roomID: 1,
-        price: '3.000.000 vnd'
-    },
-    {
-        id: 3,
-        title: 'Cho thuê phòng đẹp như khách sạn',
-        image: 'room_3.jpeg',
-        roomID: 3,
-        price: '2.000.000 vnd'
-    }
-]
+import {API_URL} from "../../config";
+import NumberFormat from "react-number-format";
+import {Badge} from "react-bootstrap";
+import NewsTable from "./News/NewsTable";
 
 function News() {
+    const [news, setNews] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(async () => {
+        if(!loading) {
+            await fetch(API_URL + "room",{ method: 'GET'}).then((response) => {
+                if(response.ok){
+                    return response.json();
+                }
+                throw response;
+            }).then(data => {
+                let listNews = data.map((val) => {
+                    return {
+                        "id": val.id,
+                        "address": val.address,
+                        "image": <img src={`/assets/images/rooms/${val.image[0].name}`} width="160"/>,
+                        "host": val.host,
+                        "price": <NumberFormat value={val.price} displayType={'text'} thousandSeparator={true} />,
+                        "status": val.status ? <Badge bg="success">Active</Badge> : <Badge bg="secondary">Inactive</Badge>,
+                        "des": val.post.title,
+                        "actions": <a href={`/admin/new/${val.id}`} className="btn btn-sm btn-dark">Chi tiết</a>
+                    }
+                });
+                setNews(listNews);
+                setLoading(true);
+            }).catch((error) => {
+                return error;
+            });
+        }
+    }, []);
+
+    useEffect(async () => {
+        if (news.length > 0 ) {
+            setLoading(true);
+        } else {
+            setLoading(false);
+        }
+    }, [news])
+
     return (
         <>
             <Header title={' Quản lý tin'}/>
@@ -31,9 +57,11 @@ function News() {
 
                 <TabPanel>
                     <div className="container py-5">
-                        {news.map((item, i) => (
-                            <CardItem src={item.image} title={item.title} subTitle={item.price} btnText={'Chi tiết'} linkBtn={`/admin/new/${item.id}`}/>
-                        ))}
+                        { loading ?
+                            <NewsTable userData={news}/>
+                            :
+                            <div>Loading...</div>
+                        }
                     </div>
                 </TabPanel>
             </Tabs>
