a8bd484574842765781eef59395a8b66f7110e5f
LOAN Ngo <mrs.loan1999@gmail.com>
2021-09-19 10:09:38 +0700

    Admin - Bill - Check UI

--- src/component/Admin/listBill.js
+++ src/component/Admin/listBill.js
@@ -128,7 +128,7 @@ function ListBills() {
             body: JSON.stringify(item)
         }).then(async function (response) {
             const result = await response.json();
-            if (result === null || result === 0) {
+            if (result == null || result == 0) {
                 setErrorMessage("Xác nhận không thành công!");
                 setAlertStatus(true);
                 setAlertType("error");
@@ -147,9 +147,6 @@ function ListBills() {
             return response;
         }).catch(function (error) {
             console.log(error);
-            setErrorMessage("Xác nhận không thành công!");
-            setAlertStatus(true);
-            setAlertType("error");
         });
     }
 
