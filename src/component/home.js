ff4fd439ae4cba6584f16f28d3f84f7377f55eb9
tylam0721 <lamnhatty2109@gmail.com>
2021-09-19 14:45:37 +0700

    add search rooms by places

--- src/component/home.js
+++ src/component/home.js
@@ -1,15 +1,22 @@
-import React, {useState, useEffect} from 'react';
-import {Carousel, Card } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Carousel, Card } from 'react-bootstrap';
 import { API_URL, CLOUD_IMG } from "../config/index";
 import NumberFormat from "react-number-format";
 
 function Home() {
     const [rooms, setRooms] = useState([]);
+    const [cities, setCities] = useState([]);
+    const [districts, setDistricts] = useState([]);
+    const [wards, setWards] = useState([]);
+    const [selectedCity, setSelectedCity] = useState({ id: 0, value: null });
+    const [selectedDistrict, setSelectedDistrict] = useState({ id: 0, value: null });
+    const [selectedWard, setSelectedWard] = useState({ id: 0, value: null });
+    const GHN_TOKEN = 'b08e0769-130e-11ec-b8c6-fade198b4859';
 
     useEffect(async () => {
         if (rooms) {
-            await fetch(API_URL + "room",{ method: 'GET'}).then((response) => {
-                if(response.ok){
+            await fetch(API_URL + "room", { method: 'GET' }).then((response) => {
+                if (response.ok) {
                     return response.json();
                 }
                 throw response;
@@ -20,7 +27,136 @@ function Home() {
             });
         }
 
+        getCities();
     }, []);
+
+    const getCities = () => {
+        fetch('https://dev-online-gateway.ghn.vn/shiip/public-api/master-data/province'
+            , {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'token': GHN_TOKEN
+                }
+            }
+        )
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                if (myJson.data) {
+                    setCities(myJson.data);
+                }
+            });
+    };
+
+    const getDistricts = () => {
+        fetch(`https://dev-online-gateway.ghn.vn/shiip/public-api/master-data/district`
+            , {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'token': GHN_TOKEN
+                }
+            }
+        )
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                var listDistrictByProvince = [];
+                myJson.data.map((item) => {
+                    if (item.ProvinceID == selectedCity.id) {
+                        listDistrictByProvince = [...listDistrictByProvince, item];
+                    }
+                });
+                setDistricts(listDistrictByProvince);
+            });
+    };
+
+    const getWards = () => {
+        console.log(selectedDistrict);
+        fetch(`https://dev-online-gateway.ghn.vn/shiip/public-api/master-data/ward?district_id=${selectedDistrict.id}`
+            , {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'token': GHN_TOKEN
+                }
+            }
+        )
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                if (myJson.data) {
+                    setWards(myJson.data);
+                }
+            });
+    };
+
+    useEffect(() => {
+        if (selectedCity) {
+            getDistricts();
+        }
+    }, [selectedCity]);
+
+    useEffect(() => {
+        if (selectedDistrict) {
+            getWards();
+        }
+    }, [selectedDistrict]);
+
+    const handleSelectCity = (value) => {
+        var valueSplitted = value.split(',');
+        setSelectedCity({
+            id: valueSplitted[0],
+            value: valueSplitted[1]
+        });
+        setWards([]);
+    };
+
+    const handleSelectDistrict = (value) => {
+        var valueSplitted = value.split(',');
+        setSelectedDistrict({
+            id: valueSplitted[0],
+            value: valueSplitted[1]
+        });
+    };
+
+    const handleSelectWard = (value) => {
+        var valueSplitted = value.split(',');
+        setSelectedWard({
+            id: valueSplitted[0],
+            value: valueSplitted[1]
+        });
+    };
+
+    const handleSearchByPlaces = () => {
+        const searchData = {
+            city: (selectedCity.value) ? selectedCity.value : "",
+            district: (selectedDistrict.value) ? selectedDistrict.value : "",
+            ward: (selectedWard.value) ? selectedWard.value : "",
+        }
+
+        console.log(searchData);
+
+        fetch(`${API_URL}room/search-room`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(searchData),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                setRooms(data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
 
     return (
         <>
@@ -69,20 +205,45 @@ function Home() {
                         <div className="row">
                             <div className="col-lg-12">
                                 <div className="row">
-                                    <div className="col-lg-6 col-12 p-0">
+                                    {/* <div className="col-lg-6 col-12 p-0">
                                         <input type="text" className="form-control search-slt"
                                                placeholder="Tìm kiếm"/>
-                                    </div>
+                                    </div> */}
                                     <div className="col-lg-3 col-12 p-0">
-                                        <select className="form-control search-slt" id="province">
-                                            <option>Chọn tỉnh</option>
-                                            <option value="1">Hồ Chí Minh</option>
-                                            <option value="2">Hà Nội</option>
-                                            <option value="3">Đà Nẵng</option>
+                                        <select className="form-control search-slt" id="province" onChange={(e) => handleSelectCity(e.target.value)}>
+                                            <option value="">Chọn tỉnh thành</option>
+                                            {
+                                                cities.map((item, index) => {
+                                                    return <option key={index}
+                                                        value={`${item.ProvinceID},${item.ProvinceName}`}>{item.ProvinceName}</option>;
+                                                })
+                                            }
                                         </select>
                                     </div>
                                     <div className="col-lg-3 col-12 p-0">
-                                        <button type="button" className="btn bg-gradient btn-danger wrn-btn">Search
+                                        <select className="form-control search-slt" id="province" onChange={(e) => handleSelectDistrict(e.target.value)}>
+                                            <option value="">Chọn quận huyện</option>
+                                            {
+                                                districts.map((item, index) => {
+                                                    return <option key={index}
+                                                        value={`${item.DistrictID},${item.DistrictName}`}>{item.DistrictName}</option>;
+                                                })
+                                            }
+                                        </select>
+                                    </div>
+                                    <div className="col-lg-3 col-12 p-0">
+                                        <select className="form-control search-slt" id="province" onChange={(e) => handleSelectWard(e.target.value)}>
+                                            <option value="">Chọn quận huyện</option>
+                                            {
+                                                wards.map((item, index) => {
+                                                    return <option key={index}
+                                                        value={`${item.WardCode},${item.WardName}`}>{item.WardName}</option>;
+                                                })
+                                            }
+                                        </select>
+                                    </div>
+                                    <div className="col-lg-3 col-12 p-0">
+                                        <button onClick={(e) => handleSearchByPlaces()} type="button" className="btn bg-gradient btn-danger wrn-btn">Search
                                         </button>
                                     </div>
                                 </div>
@@ -96,29 +257,29 @@ function Home() {
                     {rooms.filter(val => val.status === 1).map((item, i) => {
                         let img = "no-img.png";
                         if (typeof item.image !== 'undefined' && item.image.length) {
-                            if(typeof item.image[0].name !== 'undefined') {
+                            if (typeof item.image[0].name !== 'undefined') {
                                 img = item.image[0].name;
                             }
                         }
                         return (
                             <div key={i} className="col-lg-4 col-12 d-grid justify-content-center pb-5">
-                                <Card style={{width: '18rem'}}>
+                                <Card style={{ width: '18rem' }}>
                                     <Card.Img variant="top"
-                                              src={`${CLOUD_IMG}${img}`}/>
+                                        src={`${CLOUD_IMG}${img}`} />
                                     <Card.Body>
                                         <Card.Title>{item.address && item.address}</Card.Title>
                                         <Card.Text>
-                                            {item.addition_infor && item.addition_infor} <br/>
-                                            Giá: <span className="fw-bold"><NumberFormat value={item.price} displayType={'text'} thousandSeparator={true} /> (VND)</span> <br/>
+                                            {item.addition_infor && item.addition_infor} <br />
+                                            Giá: <span className="fw-bold"><NumberFormat value={item.price} displayType={'text'} thousandSeparator={true} /> (VND)</span> <br />
                                             {item.district.name &&
-                                            <>
-                                                {item.district.prefix}: {item.district.name} <br/>
-                                            </>
+                                                <>
+                                                    {item.district.prefix}: {item.district.name} <br />
+                                                </>
                                             }
                                             {item.ward.name &&
-                                            <>
-                                                {item.ward.prefix}: {item.ward.name} <br/>
-                                            </>
+                                                <>
+                                                    {item.ward.prefix}: {item.ward.name} <br />
+                                                </>
                                             }
                                             {item.province && item.province.name}
                                         </Card.Text>
